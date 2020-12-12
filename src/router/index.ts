cf63c8fba65b4bc6a831f743c6cb340186315ce7
Gem Xli <x.li.gem@gmail.com>
2020-12-12 23:09:12 +0800

    fix: export 不生效

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,2 @@
-import KoaRouter from 'koa-router'
-import { useController } from './decorator'
-import { controllers } from '../controller'
-
 export * from './decorator'
-
-export const router = new KoaRouter({ prefix: '/api' })
-
-function useRouter(target: any) {
-  const r = useController(target)
-
-  return [r.routes(), r.allowedMethods()]
-}
-
-for (const c of controllers) {
-  router.use(...useRouter(c))
-}
+export * from './router'
